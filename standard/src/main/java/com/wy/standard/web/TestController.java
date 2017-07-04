df8571d3b9cb6ed608f5552c8658219d33a00e2c
weiyang <296075945@qq.com>
2017-07-04 13:27:50 +0800

    123

---
+++ standard/src/main/java/com/wy/standard/web/TestController.java
@@ -0,0 +1,13 @@
+package com.wy.standard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello";
+	}
+}
